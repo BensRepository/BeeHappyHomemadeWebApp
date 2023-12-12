2980f0d2bdbc0e7ddac463e3de0284adb9bb09c0
Ben <thebenhodges@gmail.com>
2023-12-12 20:30:17 +0000

    duplicate detection added

--- static/js/enquire.js
+++ static/js/enquire.js
@@ -21,36 +21,14 @@ function customChoice(item){
     //document.getElementById('item_picture_value').src = "/static/images/item-types/"+folder+"/"+this.innerText+".jpg"
 }
 
+function clearList(){
+    localStorage.removeItem('students')
+    generateItemList()
+}
 function addToEnquiriesList(item,design,price){
 
     design = design.split("Selected: ")[1]
 
-    //checking for dups
-    // try{
-    //     let retString = localStorage.getItem("students") 
-    //     let storageArray = JSON.parse(retString) 
-    //     alert(storageArray)
-    //     added = 0
-    //     for (let i = 0; i < storageArray.length; i++) {
-    //         if (storageArray[i][1] == design && storageArray[i][0] == item) {
-    //             storageArray[i][2] = 2
-    //             added = 1
-    //             break
-    //         } }
-    //     if (added != 1) {
-    //         storageArray.push([item,design,1,price])
-    //        } else {
-    //         localStorage.setItem('students',JSON.stringify(storageArray))
-    //        }
-    // } catch(error){
-    //     alert("Cache empty")
-    //     storageArray = []
-    // }
-
-
-    //localStorage.setItem('students',JSON.stringify(storageArray)) 
-   
-    alert(item)
     if (item == "Pick Design") {
         alert("please pick a design!")
     } else {
@@ -58,106 +36,132 @@ function addToEnquiriesList(item,design,price){
             storageArray = localStorage.getItem("students") 
             storageArray = JSON.parse(storageArray) 
             //work out if one dimensional still
-         
+
             if (storageArray.every(entry => !Array.isArray(entry))) {
                 // One dimensional alert
-                alert("One dimensional")
-                var row = document.createElement("tr"); 
-                var name_cell = document.createElement("th"); 
-                var design_cell = document.createElement("th"); 
-                var price_cell = document.createElement("th");
-                var quantity_cell = document.createElement("th"); 
-    
-                name_cell.innerHTML = item
-                design_cell.innerHTML = design
-                price_cell.innerHTML = price
-                quantity_cell.innerHTML = 1
-                table = document.getElementById('item-list')
-                row.append(name_cell)
-                row.append(design_cell)
-                row.append(quantity_cell)
-                row.append(price_cell)
-                table.appendChild(row)
-                
-                storageArray = [[storageArray],[item,design,1,price]]
-                localStorage.setItem('students',JSON.stringify(storageArray))
-            } else {
-                alert("Two dimensional")
-                var row = document.createElement("tr"); 
-                var name_cell = document.createElement("th"); 
-                var design_cell = document.createElement("th"); 
-                var price_cell = document.createElement("th");
-                var quantity_cell = document.createElement("th"); 
-    
-                name_cell.innerHTML = item
-                design_cell.innerHTML = design
-                price_cell.innerHTML = price
-                quantity_cell.innerHTML = 1
-                table = document.getElementById('item-list')
-                row.append(name_cell)
-                row.append(design_cell)
-                row.append(quantity_cell)
-                row.append(price_cell)
-                table.appendChild(row)
-
-                storageArray.push([item,design,1,price])
-                localStorage.setItem('students',JSON.stringify(storageArray))
-
-            } // Has at least one entry that is an array
+                //alert("One dimensional")
+                //checking for dups
             
+                if ((storageArray[0] == item)&& (storageArray[1] == design)) {
+                    storageArray[2] = Number(storageArray[2]) +1
+                    localStorage.setItem('students',JSON.stringify(storageArray))
+                    
+                } else {
+                    
+                    storageArray = [[storageArray[0],storageArray[1],storageArray[2],storageArray[3]],[item,design,1,price]]
+                    localStorage.setItem('students',JSON.stringify(storageArray))
+                
+                }
+
+            } else {
+                //Check for dups
+                added = false
+                for (let i = 0; i < storageArray.length; i++) {
+                    if (storageArray[i][1] == design && storageArray[i][0] == item) {
+                        storageArray[i][2] = Number(storageArray[i][2]) + 1
+                        localStorage.setItem('students',JSON.stringify(storageArray))
+                        added = true
+                        break
+                    } else{
+
+                    }   }
+                if (added == true) {
+                    
+                }  else{
+                    alert("alert")
+                    storageArray.push([item,design,1,price])
+                    localStorage.setItem('students',JSON.stringify(storageArray))
+                }
+
+            } 
             
         } else {
-            var row = document.createElement("tr"); 
-            var name_cell = document.createElement("th"); 
-            var design_cell = document.createElement("th"); 
-            var price_cell = document.createElement("th");
-            var quantity_cell = document.createElement("th"); 
-
-            name_cell.innerHTML = item
-            design_cell.innerHTML = design
-            price_cell.innerHTML = price
-            quantity_cell.innerHTML = 1
-            table = document.getElementById('item-list')
-            row.append(name_cell)
-            row.append(design_cell)
-            row.append(quantity_cell)
-            row.append(price_cell)
-            table.appendChild(row)
-
-
+            //first entry
             localStorage.setItem('students',JSON.stringify([item,design,"1",price]))
-
         }
     }
 
+    generateItemList()
 }
 
 function generateItemList(){
-    //alert(localStorage.getItem("students"))
+    oldtable = document.getElementById("item-list")
+    oldtable.remove()
+    table = document.createElement("table"); 
+    row = document.createElement("tr"); 
+    var name_cell = document.createElement("th"); 
+    var design_cell = document.createElement("th"); 
+    var price_cell = document.createElement("th");
+    var quantity_cell = document.createElement("th"); 
+    table.id = "item-list"
+    name_cell.innerHTML = "Name"
+    design_cell.innerHTML = "Design"
+    price_cell.innerHTML = "Price"
+    quantity_cell.innerHTML = "Quantity"
+    row.append(name_cell)
+    row.append(design_cell)
+    row.append(quantity_cell)
+    row.append(price_cell)
+    table.appendChild(row)
+
+
+    document.getElementById("add-table").appendChild(table)
     if ("students" in localStorage) {
         storageArray = localStorage.getItem("students") 
         storageArray = JSON.parse(storageArray)
         table = document.getElementById("item-list")
-        for (let i = 0; i < storageArray.length; i++) {
+        if (storageArray.every(entry => !Array.isArray(entry))) {
+            // One dimensional alert
+            //alert("One dimensional")
+         
             var row = document.createElement("tr"); 
             var name_cell = document.createElement("th"); 
             var design_cell = document.createElement("th"); 
             var price_cell = document.createElement("th");
             var quantity_cell = document.createElement("th"); 
 
-            name_cell.innerHTML = storageArray[i][0]
-            design_cell.innerHTML = storageArray[i][1]
-            price_cell.innerHTML = storageArray[i][3]
-            quantity_cell.innerHTML = storageArray[i][2]
+            name_cell.innerHTML = storageArray[0]
+            design_cell.innerHTML = storageArray[1]
+            price_cell.innerHTML = storageArray[3]
+            quantity_cell.innerHTML = storageArray[2]
 
             row.append(name_cell)
             row.append(design_cell)
             row.append(quantity_cell)
             row.append(price_cell)
             table.appendChild(row)
-        }
-    } else {
+            
+      
+
+       
+        } else {
+      
+            //alert("Two dimensional")
+            for (let i = 0; i < storageArray.length; i++) {
+          
+                //alert(storageArray[i][0])
+                var row = document.createElement("tr"); 
+                var name_cell = document.createElement("th"); 
+                var design_cell = document.createElement("th"); 
+                var price_cell = document.createElement("th");
+                var quantity_cell = document.createElement("th"); 
     
+                name_cell.innerHTML = storageArray[i][0]
+                design_cell.innerHTML = storageArray[i][1]
+                price_cell.innerHTML = storageArray[i][3]
+                quantity_cell.innerHTML = storageArray[i][2]
+                row.append(name_cell)
+                row.append(design_cell)
+                row.append(quantity_cell)
+                row.append(price_cell)
+                table.appendChild(row)
+            }
+
+
+        } // Has at least one entry that is an array
+        
+    } else {
+     
     }
    
     
